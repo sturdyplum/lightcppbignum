9029da88dd2ec374d62ed36f162df697bef1cc32
sturdyplum <pabbelt@gmail.com>
2016-10-05 15:44:22 -0400

    Commented code
    Commented code

--- bigNum.cpp
+++ bigNum.cpp
@@ -4,13 +4,16 @@
 
 using namespace std;
 
-#define maxBigIntLength 2500
+#define maxBigIntLength 2500//size of numbers
 
 struct bigInt{
-    int number[maxBigIntLength];
-    bool pos;
+    int number[maxBigIntLength];//number is stored here backwards
+    bool pos;//pos = true if number is positive
 
+	//default constructor
     bigInt() {this->pos = true,  memset(this->number, 0, sizeof this->number);}
+	
+	//integer constructor
     bigInt(int i)
     {
         if(i < 0) this->pos = false;
@@ -24,6 +27,8 @@ struct bigInt{
             i/=10;
         }
     }
+	
+	//string constructor
     bigInt(string s)
     {
         this->pos= true;
@@ -36,12 +41,15 @@ struct bigInt{
         }
     }
 
+	//returns true if |this| > |other|
     bool absGreater(bigInt &other)
     {
         int i = maxBigIntLength-1;
         while(this->number[i] == other.number[i] and i >= 0)i--;
         return (this->number[i] > other.number[i]);
     }
+	
+	//adds a and b assums that they are either both positive or bot negative
     static bigInt addBig(bigInt &a, bigInt &b)
     {
         bigInt c;
@@ -53,6 +61,7 @@ struct bigInt{
         return c;
     }
 
+	//subtracts b from a assumes that both are positive and that a is greater than b 
     static bigInt subBig(bigInt &a, bigInt &b)
     {
         bigInt c;
@@ -65,14 +74,16 @@ struct bigInt{
         return c;
     }
 
+	//handles addition operator
     bigInt operator+(bigInt& other)
     {
+		//if both are positive or both are negative then just add and make the sign equal to either
         if((this->pos && other.pos)||(!this->pos && !other.pos))
         {
             bigInt c = addBig(*this,other);
             c.pos = other.pos;
             return c;
-        }
+        }//if one is positive and one is negative, subtract the one with the smallest absolute value from the one with the greatest and then make the sign equal to whatever the greatest is
         else if(this->absGreater(other))
         {
             bigInt c = subBig(*this,other);
@@ -90,6 +101,7 @@ struct bigInt{
 
     bigInt operator-(bigInt& other)
     {
+		//switch the sign of other then add then switch it back
         other.pos = !other.pos;
         bigInt c = *this + other;
         other.pos = !other.pos;
@@ -97,7 +109,7 @@ struct bigInt{
     }
 
     bigInt operator*(bigInt& other)
-    {
+    {//warning do not multiply 2 numbers whos n is > maxBigIntLength/2 or you will get a wrong answer
         bigInt c;
         c.pos = (other.pos or this->pos) or (!other.pos and !this->pos);
         for(int i = 0; i < maxBigIntLength/2; i++)
@@ -137,7 +149,7 @@ struct bigInt{
 
 
     bool operator==(bigInt &other)
-    {
+    {//if signs are not equal then return false otherwise step through number array making sure all indicies are equal
         if(this->pos != other.pos) return false;
         for(int  i = 0; i < maxBigIntLength; i++)
         {
@@ -147,7 +159,7 @@ struct bigInt{
     }
 
     bool operator>(bigInt &other)
-    {
+    {//check signs then check right most non equal number in the number array
         if(this->pos and !other.pos) return true;
         if(other.pos and !this->pos) return false;
         if(this->pos and other.pos) return this->absGreater(other);
@@ -155,13 +167,13 @@ struct bigInt{
     }
 
     bool operator<(bigInt &other)
-    {
+    {//flip and check that also not equal too (could probably speed this up if implemented on its own since you wouldn't have to run equals)
         if(!(*this == other) and !(*this > other)) return true;
         return false;
     }
 
     void print()
-    {
+    {//prints out the big int without leading zeros
         int i = maxBigIntLength-1;
         if(!this->pos) printf("-");
         while(this->number[i] == 0 and i>=0) i--;
